b3b48abd0b645ee7c66b8ae70f3651759dd87ae9
Ali A. Samir <aly1744@hotmail.com>
2025-03-18 12:46:38 +0200

    Update build.rs file to add better path definition and error handling

--- YOLOv8-ONNXRuntime-Rust/build.rs
+++ YOLOv8-ONNXRuntime-Rust/build.rs
@@ -1,8 +1,14 @@
+use std::env;
 use std::path::Path;
 
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env::set_var("PROTOC", r"C:\protoc-30.1-win64\bin\protoc.exe");
     let proto_dir = Path::new("proto");
     let proto_file = proto_dir.join("result.proto");
-    tonic_build::compile_protos(proto_file)?;
+    tonic_build::compile_protos(proto_file).map_err(|e| {
+        eprintln!("Failed to compile protos: {}", e);
+        e
+    })?;
     Ok(())
 }
