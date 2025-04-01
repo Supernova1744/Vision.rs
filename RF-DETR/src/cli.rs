95b2f6b27f8b0186d3bf613e2ce6b3e64c741a57
Ali A. Samir <aly1744@hotmail.com>
2025-04-01 14:11:58 +0200

    Add new configs to cli

--- RF-DETR/src/cli.rs
+++ RF-DETR/src/cli.rs
@@ -8,7 +8,6 @@ pub struct Args {
     #[arg(long, required = true)]
     pub model: String,
 
-
     /// using CUDA EP
     #[arg(long)]
     pub cuda: bool,
@@ -21,5 +20,26 @@ pub struct Args {
 
     #[arg(long, default_value_t = String::from(r"output\"))]
     pub output: String,
+
+    #[arg(long, default_value_t = 560)]
+    pub img_w: usize,
+
+    #[arg(long, default_value_t = 560)]
+    pub img_h: usize,
+    
+    #[arg(long, default_value_t = 0.5)]
+    pub conf_th: f32,
+    
+    #[arg(long, default_value_t = 0.25)]
+    pub iou_th: f32,
+    
+    #[arg(skip = 3)]
+    pub ch: i32,
+
+    #[arg(skip = [0.485, 0.456, 0.406])]
+    pub mean: [f32; 3],
+
+    #[arg(skip = [0.229, 0.224, 0.225])]
+    pub std: [f32; 3],
 
 }
