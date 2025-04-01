386c39119f01b4ff76dd9a3482379ae00a653691
Ali A. Samir <aly1744@hotmail.com>
2025-04-01 14:16:43 +0200

    Update main module

--- RF-DETR/src/main.rs
+++ RF-DETR/src/main.rs
@@ -1,24 +1,32 @@
 use clap::Parser;
-
 use tonic::transport::Server;
-use RF_DETR::{Processor, PreprocessConfig, OnnxModel, Args};
-
 use RF_DETR::service::MyImageProcessor;
+use RF_DETR::{PreProcessor, PostProcessor, OnnxModel, Args};
 use RF_DETR::grpc::image_processor_server::ImageProcessorServer;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Start gRPC server
+    // Load the command line arguments
     let args = Args::parse();
+    // Define gRPC server address
     let addr = "[::1]:50051".parse()?;
-    let processor = Processor::new(PreprocessConfig::default());
+    // Load the model, preprocessors, and postprocessors
     let model = OnnxModel::new(args.clone().cuda).load_model(&args.model)?;
-    let x = ImageProcessorServer::new(MyImageProcessor::new(model, processor, args));
-    println!("ImageProcessor server listening on {}", addr);
+    let preprocessor = PreProcessor::new(args.clone());
+    let postprocessor = PostProcessor::new(args.clone());
+    
+    let server = ImageProcessorServer::new(
+        MyImageProcessor::new(
+            model,
+            preprocessor,
+            postprocessor,
+            args)
+        );
     Server::builder()
-    .add_service(x)
+    .add_service(server)
     .serve(addr)
     .await?;
+    println!("RF-DETR Object Detection server listening on {}", addr);
 
     Ok(())
 }
