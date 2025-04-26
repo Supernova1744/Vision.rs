aeb307b8529083df24fac19a57cb9485aecdacaf
Ali A. Samir <aly1744@hotmail.com>
2025-04-26 22:18:37 +0300

    Update cv2 and cv3 structure

--- yolov8_train/src/head.rs
+++ yolov8_train/src/head.rs
@@ -6,16 +6,24 @@ use crate::conv::ConvBlock;
 use crate::dfl::DFL;
 use num_integer::Integer;
 
+enum Block<B: Backend> {
+    ConvBlock(ConvBlock<B>),
+    Conv2d(Conv2d<B>)
+}
+
+impl <B: Backend> Block<B> {
+    pub fn forward(&self, input: Tensor<B, 4>) -> Tensor<B, 4> {
+        match self {
+            Block::ConvBlock(conv_block) => conv_block.forward(input),
+            Block::Conv2d(conv2d) => conv2d.forward(input)
+        }
+    }
+    
+}
 
 pub struct Detect<B: Backend> {
-    cv2_seq1: Vec<ConvBlock<B>>,
-    cv2_seq2: Vec<ConvBlock<B>>,
-    cv2_seq3: Vec<Conv2d<B>>,
-    cv3_seq1_b1: Vec<ConvBlock<B>>,
-    cv3_seq1_b2: Vec<ConvBlock<B>>,
-    cv3_seq2_b1: Vec<ConvBlock<B>>,
-    cv3_seq2_b2: Vec<ConvBlock<B>>,
-    cv3_seq3_b1: Vec<Conv2d<B>>,
+    cv2: Vec<Vec<Block<B>>>,
+    cv3: Vec<Vec<Block<B>>>,
     dfl: DFL<B>,
     stride: Tensor<B, 1>,
     strides: Tensor<B, 3>,
@@ -43,44 +51,34 @@ impl <B: Backend> Detect<B> {
         let anchors = Tensor::<B, 3>::empty([1,1,1], device);
         let strides = Tensor::<B, 3>::empty([1,1,1], device);
 
+        let mut cv2 = Vec::<Vec<Block<B>>>::with_capacity(nl);
+        let mut cv3 = Vec::<Vec<Block<B>>>::with_capacity(nl);
 
-        let mut cv2_seq1 = Vec::<ConvBlock<B>>::with_capacity(nl);
-        let mut cv2_seq2 = Vec::<ConvBlock<B>>::with_capacity(nl);
-        let mut cv2_seq3    = Vec::<Conv2d<B>>::with_capacity(nl);
-        
-        let mut cv3_seq1_b1 = Vec::<ConvBlock<B>>::with_capacity(nl);
-        let mut cv3_seq1_b2 = Vec::<ConvBlock<B>>::with_capacity(nl);
-        let mut cv3_seq2_b1 = Vec::<ConvBlock<B>>::with_capacity(nl);
-        let mut cv3_seq2_b2 = Vec::<ConvBlock<B>>::with_capacity(nl);
-        let mut cv3_seq3_b1    = Vec::<Conv2d<B>>::with_capacity(nl);
-
-        for i in 0..nl{
-            cv2_seq1.push(ConvBlock::<B>::new(ch[i], c2, 3, 1, None, None, None, device));
-            cv2_seq2.push(ConvBlock::<B>::new(c2, c2, 3, 1, None, None, None, device));
-            cv2_seq3.push(Conv2dConfig::new([c2, 4 * reg_max], [1, 1]).with_bias(true).init(device));
-
-            cv3_seq1_b1.push(ConvBlock::<B>::new(ch[i], ch[i], 3, 1, None, Some(ch[i].gcd(&(ch[i] as usize))), None, device));
-            cv3_seq1_b2.push(ConvBlock::<B>::new(ch[i], c3, 1, 1, None, None, None, device));
-
-            cv3_seq2_b1.push(ConvBlock::<B>::new(c3, c3, 3, 1, None, Some(c3.gcd(&(c3 as usize))), None, device));
-            cv3_seq2_b2.push(ConvBlock::<B>::new(c3, c3, 1, 1, None, None, None, device));
-
-            cv3_seq3_b1.push(Conv2dConfig::new([c3, nc], [1, 1]).with_bias(true).init(device));
+        for i in 0..nl {
+            cv2.push(vec![
+                Block::ConvBlock(ConvBlock::<B>::new(ch[i], c2, 3, 1, None, None, None, device)),
+                Block::ConvBlock(ConvBlock::<B>::new(c2, c2, 3, 1, None, None, None, device)),
+                Block::Conv2d(Conv2dConfig::new([c2, 4 * reg_max], [1, 1]).with_bias(true).init(device))
+            ]);
+            
+            cv3.push(vec![
+                Block::ConvBlock(ConvBlock::<B>::new(ch[i], ch[i], 3, 1, None, Some(ch[i].gcd(&(ch[i] as usize))), None, device)),
+                Block::ConvBlock(ConvBlock::<B>::new(ch[i], c3, 1, 1, None, None, None, device)),
+                Block::ConvBlock(ConvBlock::<B>::new(c3, c3, 3, 1, None, Some(c3.gcd(&(c3 as usize))), None, device)),
+                Block::ConvBlock(ConvBlock::<B>::new(c3, c3, 1, 1, None, None, None, device)),
+                Block::Conv2d(Conv2dConfig::new([c3, nc], [1, 1]).with_bias(true).init(device))
+            ]);
+                    
         }
+      
         
         let stride = Tensor::<B, 1>::zeros([nl], device);
 
         let dfl = DFL::<B>::new(reg_max, device);
 
         Self {
-            cv2_seq1,
-            cv2_seq2,
-            cv2_seq3,
-            cv3_seq1_b1,
-            cv3_seq1_b2,
-            cv3_seq2_b1,
-            cv3_seq2_b2,
-            cv3_seq3_b1,
+            cv2,
+            cv3,
             dfl,
             stride,
             strides,
@@ -99,30 +97,20 @@ impl <B: Backend> Detect<B> {
         }
     }
 
-    
-
     pub fn forward(&self, x: Vec<Tensor<B, 4>>) -> Vec<Tensor<B, 4>> {
         let mut out = Vec::with_capacity(self.nl);
-        x.iter()
-         .enumerate()
-         .for_each(|(i, xi)| {
-            let y1 = self.cv2_seq3[i].forward(
-                self.cv2_seq2[i].forward(
-                    self.cv2_seq1[i].forward(xi.clone())
-                )
-             );
-            let y2 = self.cv3_seq3_b1[i].forward(
-                self.cv3_seq2_b2[i].forward(
-                    self.cv3_seq2_b1[i].forward(
-                        self.cv3_seq1_b2[i].forward(
-                            self.cv3_seq1_b1[i].forward(xi.clone())
-                        )
-                    )
-                )
-             );
-             out.push(Tensor::<B, 4>::cat(vec![y1, y2], 1));
-         });
-    
+        for (i, xi) in x.into_iter().enumerate() {
+            
+            let y1 = self.cv2[i]
+                .iter()
+                .fold(xi.clone(), |acc, layer| layer.forward(acc));
+            
+            let y2 = self.cv3[i]
+                .iter()
+                .fold(xi, |acc, layer| layer.forward(acc));
+            
+            out.push(Tensor::cat(vec![y1, y2], 1));
+        }
         out
     }
 
@@ -165,7 +153,43 @@ impl <B: Backend> Detect<B> {
         output
         
     }
-    
+    // Convert the following python function to rust
+    // def postprocess(preds: torch.Tensor, max_det: int, nc: int = 80):
+    //     """
+    //     Post-processes YOLO model predictions.
+
+    //     Args:
+    //         preds (torch.Tensor): Raw predictions with shape (batch_size, num_anchors, 4 + nc) with last dimension
+    //             format [x, y, w, h, class_probs].
+    //         max_det (int): Maximum detections per image.
+    //         nc (int, optional): Number of classes. Default: 80.
+
+    //     Returns:
+    //         (torch.Tensor): Processed predictions with shape (batch_size, min(max_det, num_anchors), 6) and last
+    //             dimension format [x, y, w, h, max_class_prob, class_index].
+    //     """
+    //     batch_size, anchors, _ = preds.shape  # i.e. shape(16,8400,84)P
+    //     boxes, scores = preds.split([4, nc], dim=-1)
+    //     index = scores.amax(dim=-1).topk(min(max_det, anchors))[1].unsqueeze(-1)
+    //     boxes = boxes.gather(dim=1, index=index.repeat(1, 1, 4))
+    //     scores = scores.gather(dim=1, index=index.repeat(1, 1, nc))
+    //     scores, index = scores.flatten(1).topk(min(max_det, anchors))
+    //     i = torch.arange(batch_size)[..., None]  # batch indices
+    //     return torch.cat([boxes[i, index // nc], scores[..., None], (index % nc)[..., None].float()], dim=-1)
+
+    // fn postprocess(&self, preds: Tensor<B, 3>, max_det: usize, nc: usize) -> Tensor<B, 4> {
+    //     let [batch_size, anchors, _] = preds.dims();
+    //     let splits = preds.split_with_sizes([4, nc].to_vec(), 2);
+    //     let (boxes, scores) = (splits[0], splits[1]);
+    //     let index = scores.max_dim(2).topk(max_det.min(anchors), 2).slice([1..2, 0..batch_size as usize, 0..anchors as usize]).unsqueeze();
+    //     let x = index.clone().repeat(&[1, 1, nc]);
+    //     let boxes = boxes.gather(2, index.int().repeat(&[1, 1, 4]));
+    //     let scores = scores.gather(2, index.int().repeat(&[1, 1, nc]));
+    //     let scores = scores.flatten(1,1).topk(max_det.min(anchors), 0).slice([0..batch_size as usize, 0..max_det as usize]).unsqueeze();
+    //     let i = Tensor::<B, 1, Int>::arange(0..batch_size as i64, self.stride.device()).reshape([batch_size as usize, 1]);
+    //     Tensor::<B, 4>::cat(vec![boxes.gather(0, i), scores.unsqueeze(), (index.float() % nc).unsqueeze()], -1)
+    // }
+        
     
     
 }
